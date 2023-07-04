3c0d2ed408568206fafdc7a82d0f707d6541c7e5
ivan.stefanov <ivanstefanov.it@gmail.com>
2023-07-04 11:12:27 +0300

    Add physics

--- script.js
+++ script.js
@@ -51,9 +51,13 @@ window.addEventListener("load", function(){
             this.collisionX += this.speedX * this.speedModifier;
             this.collisionY += this.speedY * this.speedModifier;
             this.game.obstacles.forEach(obstacle => {
-                if (this.game.checkCollision(this, obstacle)) {
-                    console.log("collision");
-                };
+                let [collision ,distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, obstacle);
+                if (collision) {
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
+                    this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
+                }
             })
         }
     }
@@ -123,9 +127,9 @@ window.addEventListener("load", function(){
         }
 
         render(context){
+            this.obstacles.forEach(obstacle => obstacle.draw(context));
             this.player.draw(context);
             this.player.update();
-            this.obstacles.forEach(obstacle => obstacle.draw(context));
         }
 
         checkCollision(a, b){
@@ -133,7 +137,7 @@ window.addEventListener("load", function(){
             const dy = a.collisionY - b.collisionY;
             const distance = Math.hypot(dy, dx);
             const sumOfRadii = a.collisionRadius + b.collisionRadius;
-            return(distance < sumOfRadii);
+            return [(distance < sumOfRadii),distance, sumOfRadii, dx, dy];
         }
 
         init(){
